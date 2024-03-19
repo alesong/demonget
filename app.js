56ce9bd744b0e6a8e69c6222dfa9d10abb2dd142
Alex <alesong@hotmail.com>
2024-03-19 16:17:39 -0500

    Se corrige el estino GET con ping

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const intervalo = 40 * 1000; // 40 segundos
 setInterval(async () => {
     try {
         //const response = await axios.get('http://localhost:3000/ping');
-        const response = await axios.get('https://control-vencimientos.onrender.com');
+        const response = await axios.get('https://control-vencimientos.onrender.com/ping');
         console.log(`Petición realizada a las ${moment().tz('America/Bogota').format('YYYY-MM-DD HH:mm:ss')}`);
         console.log(response.data);
     } catch (error) {
