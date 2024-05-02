3da648eb4e3ef89a00348e8b8d56a7a92cb95380
Alex <alesong@hotmail.com>
2024-05-02 15:50:53 -0500

    Se reduce la solicitud de ping a 2 minutos

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 4000;
 
 // Define el intervalo de tiempo en segundos
-const intervalo = 4 * 60 * 1000; // 4 minutos
+const intervalo = 2 * 60 * 1000; // 2 minutos
 
 setInterval(async () => {
     try {
